e6c1267caba554d0cbba4a157c25e4193fb6cd09
toanphp <tadangtoan@hotmail.com>
2016-08-08 10:43:36 +0700

    atasdfasdf

--- application/controllers/datacenters.php
+++ application/controllers/datacenters.php
@@ -2,7 +2,7 @@
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Datacenters extends CI_Controller
-{
+{//-----------------------------------
 	
 	public function __construct()
 	{
